0aad93e1c6cfa32cf1703730fb0868d4aeaca405
Sam L <samlitowitz@users.noreply.github.com>
2019-05-10 09:14:45 -0400

    Add documentation for the StringStack

--- pkg/protoc-gen-graphql/generator/string_stack.go
+++ pkg/protoc-gen-graphql/generator/string_stack.go
@@ -1,26 +1,34 @@
 package generator
 
+// An EmptyStackError occurs when attempting to preform an operation
+// which is not viable with an empty stack, e.g. `Pop`.
 type EmptyStackError struct{}
 
 func (e *EmptyStackError) Error() string {
 	return "value expected, stack is empty"
 }
 
+// A StringStack is stack of string elements.
 type StringStack []string
 
+// Empty determines if a stack contains no elements
 func (s StringStack) Empty() bool {
 	return len(s) == 0
 }
 
+// Peek returns the first element without altering the stack.
 func (s StringStack) Peek() string {
 	l := len(s)
 	return s[l-1]
 }
 
+// Push adds a new element to the top of the stack
 func (s StringStack) Push(v string) StringStack {
 	return append(s, v)
 }
 
+// Pop removes the top element of the stack, returning the modified stack,
+// the element, and an EmptyStackError if the stack contains no elements.
 func (s StringStack) Pop() (StringStack, string, error) {
 	l := len(s)
 	if l == 0 {
